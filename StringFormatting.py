0967d3fa640db2787c13b6789123c5459e9cef5e
Administrator <yan.beker@list.ru>
2021-11-09 10:31:52 +0200

    small changes

---
+++ StringFormatting.py
@@ -0,0 +1,16 @@
+# string formatting
+
+if __name__ == '__main__':
+    strhw = "Hello World"
+    print("My first programm {}".format(strhw))
+
+strhw0 = "Hello"
+strhw1 = "World"
+strhw2 = "HW"
+print("My first programm {0} {1} {2}".format(strhw0, strhw1, strhw2))
+print("My first programm {2} {1} {0}".format(strhw0, strhw1, strhw2))
+
+pi = 3.1415
+print("it's PI string formatting:{pi:1.2f}".format(pi=pi))  # is's PI 3.14
+
+print("My first programm {strhw0} {strhw1} {strhw2}")
---
+++ globals.py
@@ -0,0 +1,12 @@
+# {'__name__': '__main__',
+# '__doc__': None,
+# '__package__': None,
+# '__loader__': <_frozen_importlib_external.SourceFileLoader object at 0x000001746DF902E0>,
+# '__spec__': None,
+# '__annotations__': {},
+# '__builtins__': <module 'builtins' (built-in)>,
+# '__file__': 'C:\\Users\\blabla\\globals.py',
+# '__cached__': None}
+
+if __name__ == "__main__":
+    print(globals())  # print globals
