e05c2d08c210d1c6fa60cd8c8786fc9f20c34731
Administrator <yan.beker@list.ru>
2021-11-09 14:31:03 +0200

    small changes

--- FileOperations.py
+++ FileOperations.py
@@ -1,1 +1,16 @@
 # file operations
+# The key function for working with files in Python is the open() function.
+# The open() function takes two parameters; filename, and mode.
+# There are four different methods (modes) for opening a file:
+# "r" - Read - Default value. Opens a file for reading, error if the file does not exist
+# "a" - Append - Opens a file for appending, creates the file if it does not exist
+# "w" - Write - Opens a file for writing, creates the file if it does not exist
+# "x" - Create - Creates the specified file, returns an error if the file exists
+
+# script
+# check file exist, if exist then delete file, create file
+# write text in file "bla bla bla"
+# read the file and print text
+# add text in file "bla2 bla2 bla2"
+# read the file and print text
+# delete file :) relax
