3932d89a25343b9f2a2f453dc5b1448ae7fad892
Administrator <yan.beker@list.ru>
2021-11-10 20:01:39 +0200

    small changes

--- List.py
+++ List.py
@@ -1,4 +1,30 @@
 # List collection
+# Python Lists
+# Python Lists
+# Access List Items
+# Change List Items
+# Add List Items
+# Remove List Items
+# Loop Lists
+# List Comprehension
+# Sort Lists
+# Copy Lists
+# Join Lists
+
+# Method	Description
+# append()	Adds an element at the end of the list
+# clear()	Removes all the elements from the list
+# copy()	Returns a copy of the list
+# count()	Returns the number of elements with the specified value
+# extend()	Add the elements of a list (or any iterable), to the end of the current list
+# index()	Returns the index of the first element with the specified value
+# insert()	Adds an element at the specified position
+# pop()	Removes the element at the specified position
+# remove()	Removes the item with the specified value
+# reverse()	Reverses the order of the list
+# sort()	Sorts the list
+
+
 n = 0
 int_list = [1, 32, 35, 41, 59, 65, 73, 822, 922, 101]
 mixed_list = [1, 2.0, 'string', 4, "more", 6, 7, 'HelloWorld', 9, 10]
