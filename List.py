10b67eaada0ddbe150a12a4dad137ea4d0b55977
Administrator <yan.beker@list.ru>
2021-11-10 19:44:49 +0200

    small changes

--- List.py
+++ List.py
@@ -1,10 +1,22 @@
 # List collection
 n = 0
-int_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+int_list = [1, 32, 35, 41, 59, 65, 73, 822, 922, 101]
 mixed_list = [1, 2.0, 'string', 4, "more", 6, 7, 'HelloWorld', 9, 10]
 
 big_list = mixed_list + int_list
 print(f'Print big list {big_list}')
+
+big_list.append('HelloWord2')
+print(f'Print big list add {big_list}')
+
+big_list.pop(-1)
+print(f'Print big list pop -1 {big_list}')
+
+big_list.pop(1)  # index = 1
+print(f'Print big list pop index 1  {big_list}')
+
+int_list.sort()
+print(f'Print sort {int_list}')
 
 print(len(int_list))
 print(len(mixed_list))
