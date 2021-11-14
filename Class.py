3347201cba723c034c59e93e83ca662da8f40c65
Administrator <yan.beker@list.ru>
2021-11-14 14:20:00 +0200

    small changes

---
+++ Class.py
@@ -0,0 +1,11 @@
+# Class
+
+class SomeClass(object):
+    @staticmethod
+    def hello():
+        print("Hello, world")
+
+
+SomeClass.hello()  # Hello, world
+obj = SomeClass()
+obj.hello()  # Hello, world
