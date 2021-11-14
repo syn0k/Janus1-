a190b9a831e75020c92153bdf66711d5ae30d2fa
Administrator <yan.beker@list.ru>
2021-11-14 21:35:29 +0200

    small changes

--- Home_Work.py
+++ Home_Work.py
@@ -5,21 +5,23 @@
 # - For example, the user entered the number 2. Then we output:
 # *
 # **
-try:
-    PleaseInput = input('Please input the number\n')
-    # print(type(PleaseInput))
-    PleaseInput_int = int(PleaseInput)
-    # print(type(PleaseInput_int))
 
+print('#My decision task 1')
+try:
+    PleaseInput_int = int(input('Please input the number\n'))
     ch = ''
     x = 0
     while x < PleaseInput_int:
         x += 1
         ch += "*"
         print(ch)
-
 except ValueError:
     print("That's not an int!\nPlease input the number\n")
+
+print('#Teacher''s decision task 1')
+PleaseInput_int = int(input('Please input the number\n'))
+for x in range(PleaseInput_int):
+    print('*' * (x + 1))
 
 # Prompt the user to enter a number. Construct a cycle from 0 to the entered number (inclusive)
 # and for even numbers output that they are even, and for odd ones that they are odd. Output example:
@@ -27,6 +29,7 @@ except ValueError:
 # 1 is ODD
 # 2 is EVEN
 # etc...
+print('#My decision task 2')
 try:
     PleaseInput = input('Please input the number, We will check even or odd  \n')
     # debug print(type(PleaseInput))
@@ -43,3 +46,11 @@ try:
 
 except ValueError:
     print("That's not an int!\nPlease input the number\n")
+
+print('#Teacher''s decision task 2')
+PleaseInput_int = int(input('Please input the number\n'))
+for x in range(PleaseInput_int + 1):
+    if x % 2 == 0:
+        print(f'{x} is EVEN')
+    else:
+        print(f'{x} is ODD')
