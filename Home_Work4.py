26903260547d6c77ffd049f049e48fe94311b05a
Administrator <yan.beker@list.ru>
2021-11-16 16:32:02 +0200

    small changes

---
+++ Home_Work4.py
@@ -0,0 +1,5 @@
+# Write a rock-paper-scissors game against the computer.
+# Run the game in an endless loop. Request user input (R for rock, S for scissors, P for paper).
+# Generate a random selection of a computer. Display computer selection.
+# Determine the winner by displaying the relevant information.
+# Ask the user if he wants to repeat the game. If he wants to repeat, he does not want to leave the cycle.
