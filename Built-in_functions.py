f55197cfb7f71d5a4bb804b51d742cd949811d64
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-16 20:14:18 +0200

    add file

---
+++ Built-in_functions.py
@@ -0,0 +1,22 @@
+abs(-1)
+max()
+min()
+pow()
+r_ = round(3.1415,1)
+print(r_)
+
+s_ = sum([1,2,3,4,5])
+print(s_)
+
+hs= hex(42) #16
+os = oct(42) #0o 6
+bs = bin(42) # 0b bin
+print(hs)
+print(os)
+print(bs)
+
+
+all()
+
+
+
