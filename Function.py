4e907c5c95244f991bab5651ba0463de7077cd65
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-16 21:36:50 +0200

    add file

--- Function.py
+++ Function.py
@@ -5,3 +5,24 @@ def print_message(msg):
     print(msg)
 
 print_message("Hello World")
+
+
+def print_message(name):
+    return "Hello world " + name
+msg=print_message("Jim")
+print(msg)
+
+def print_message(age):
+    return age > 18
+msg=print_message(19)
+print(msg)
+
+def print_message(text):
+    return text==text[::-1]
+msg=print_message('aabaa')
+msg=print_message('aabdaa')
+print(msg)
+
+
+
+
