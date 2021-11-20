c98eefa8d876c879c2b9b2aceb921e7f5a0d9c3c
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-20 12:19:16 +0200

    A lambda function

--- Lambda.py
+++ Lambda.py
@@ -3,3 +3,24 @@
 #
 # Syntax
 # lambda arguments : expression
+
+x = lambda a: a + 10
+print(x(5))
+
+x = lambda a, b: a * b
+print(x(5, 6))
+
+
+def square(number):
+    return number*number
+
+numbers = [1,2,3,4,5,6,7]
+pr_sq=map(square, numbers)
+print(pr_sq)
+
+for x in map(square, numbers):
+    print(x)
+
+pr_list=list(map(square, numbers))
+print(pr_list)
+
