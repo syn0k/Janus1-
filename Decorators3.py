df93db6430e8bcffae132ad5a806d969c28ce472
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-20 19:00:28 +0200

    A lambda function

--- Decorators3.py
+++ Decorators3.py
@@ -1,3 +1,4 @@
+print('Exapmle 1'*3)
 def message_print(func):
     def wrapper(*args, **kwargs):
         print('Head message')
@@ -18,8 +19,8 @@ def function_print_society(inst, tel, vk, yt, tv):
 
 function_print_society('https://www.instagram.com/py_lounge', 'https://t.me/pylounge', 'https://vk.com/pylounge',
               'https://www.youtube.com/channel/UCru5', 'https://twitter.com/pylounge')
-print('*'*50)
 
+print('Exapmle 2'*3)
 def message_print(hi_mess, by_mess):
     def message_1(func):
         def wrapper(*args, **kwargs):
@@ -45,3 +46,35 @@ function_print_society('https://www.instagram.com/py_lounge', 'https://t.me/pylo
 
 
 
+
+print('Exapmle 3'*3)
+def i_am_top(func):
+    def wrapper(*args, **kwargs):
+        print('I am TOP')
+        func(*args, **kwargs)
+    return wrapper
+
+
+def message_print(hi_mess, by_mess):
+    def message_1(func):
+        def wrapper(*args, **kwargs):
+            print(hi_mess)
+            func(*args, **kwargs)
+            print(by_mess)
+        return wrapper
+    return message_1
+
+@i_am_top
+@message_print('Head message','Tail message')
+def function_print_society(inst, tel, vk, yt, tv):
+    print(f'''Society Links
+        🚀 Социальные сети:
+        ✅ Instagram: {inst}
+        ✅ Telegram: {tel}
+        ✅ Группа ВКонтакте: {vk}
+        ✅ Канал PyLounge: {yt}
+        ✅ Twitter: {tv}''')
+
+
+function_print_society('https://www.instagram.com/py_lounge', 'https://t.me/pylounge', 'https://vk.com/pylounge',
+              'https://www.youtube.com/channel/UCru5', 'https://twitter.com/pylounge')
