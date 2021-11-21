b53ea6bd206190631c0147b5acb0a89a992f31f8
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-21 12:50:11 +0200

    A lambda function

--- Home_Work5.py
+++ Home_Work5.py
@@ -16,3 +16,18 @@ def calc_dice_scores(lst):
 def any_duplicates(square):
     plain = [i for z in square for i in x]
     return sorted(plain) != [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+number_of_sticks = 10
+player_turn = 1
+while number_of_sticks > 0:
+    print(f'How many sticks you take? Remainning {number_of_sticks}')
+    taken = int(input())
+
+    if taken < 1 or taken > 3:
+        print(f'You tried to take {taken}. Allowed to take 1,2,3 sticks')
+        continue
+    number_of_sticks-=taken
+    print(f'Sticks taken: {taken}')
+    if
+
+
