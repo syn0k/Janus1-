89f61758412d43f2b9bc2e93e9dc98fdf2ac2bd2
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-21 14:02:28 +0200

    A lambda function

--- Error_handling_basic.py
+++ Error_handling_basic.py
@@ -1,5 +1,25 @@
 #Error handling basic
-print('Error handling basic')
+print('Error handling basic: Example: 1')
+
+def divide(a,b):
+    try:
+        return a/b
+    except ZeroDivisionError as ex:
+        print(f'On error occured: {ex}')
+    except:
+        print('Error occured')
+
+print('Error handling basic: Example: 1')
+try:
+  f = open("demofile.txt")
+  try:
+    f.write("Lorum Ipsum")
+  except:
+    print("Something went wrong when writing to the file")
+  finally:
+    f.close()
+except:
+  print("Something went wrong when opening the file")
 
 #Throwing exceptions. Custom types of exceptions.
 print('Throwing exceptions. Custom types of exceptions.')
