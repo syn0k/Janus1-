1fd5f16d809c1234e008e4dc167ce884847331ef
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-21 17:18:03 +0200

    A lambda function

---
+++ Unit testing basics/main_code_test.py
@@ -0,0 +1,24 @@
+import unittest
+import code_testing
+
+class FizzBuzzTest(unitTest.TestCase):
+
+    def test_fizz(self):
+        number=6
+        result=code_testing.get_reply(number)
+        self.assertEqual(result, 'Fizz')
+
+    def test_buzz(self):
+        number = 10
+        result = code_testing.get_reply(number)
+        self.assertEqual(result, 'Buzz')
+
+    def test_fizzbuzz(self):
+        number = 15
+        result = code_testing.get_reply(number)
+        self.assertEqual(result, 'FizzBuzz')
+
+if __name__ == '__main__':
+    unittest.main()
+
+
