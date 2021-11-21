18b5ad440b172f5acd5f8d5449909880dec5ea38
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-21 13:19:57 +0200

    A lambda function

---
+++ Error_handling_basic.py
@@ -0,0 +1,9 @@
+#Error handling basic
+print('Error handling basic')
+
+#Throwing exceptions. Custom types of exceptions.
+print('Throwing exceptions. Custom types of exceptions.')
+
+#Unit testing basics
+print('Unit testing basics')
+
