65d232b76b3366dd7bc45cc8680ce87c58463c29
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-21 18:02:02 +0200

    A lambda function

--- Unit testing basics/Example 1/code_testing.py
+++ Unit testing basics/Example 1/code_testing.py
@@ -1,5 +1,9 @@
 from math import pi
 
 def circle_area(radius):
+    if type(radius) not in [int, float]:
+        raise ValueError("Radius must be non-negativ real number only")
+    if radius < 0:
+        raise ValueError("Radius can't be negative")
     return pi*radius**2
 
