213ba0679f9bf29aacc7980d003e345c86245ed3
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-26 18:07:51 +0200

    add example to file Magic_methods.py

---  Object Oriented Programming in Python/Magic_methods.py
+++  Object Oriented Programming in Python/Magic_methods.py
@@ -1,1 +1,19 @@
 #Magic methods
+
+class Lion:
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f'The object Lion {self.name}'
+
+    # def __str__(self):
+    #     return f'Lion {self.name}'
+
+w = Lion('Odin')
+print(w)
+
+
+
+
+
