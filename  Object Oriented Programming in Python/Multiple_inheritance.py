9f5221cabcb2308e3671889469ff3b0439af185b
syn0k <93881789+syn0k@users.noreply.github.com>
2021-11-29 22:58:15 +0200

    add example to file Magic_methods.py

---  Object Oriented Programming in Python/Multiple_inheritance.py
+++  Object Oriented Programming in Python/Multiple_inheritance.py
@@ -1,1 +1,33 @@
 #Multiple inheritance
+
+#perent class
+class Person:
+    def can_breathe(self):
+        print('I can breathe')
+
+    def can_walk(self):
+        print('I can walk')
+
+#sub class
+class Doctor(Person):
+    def can_cure(self):
+        print('HI, I am Doctor')
+
+class Architect(Person):
+    def can_build(self):
+        print('HI, I am build')
+
+a = Architect()
+a.can_walk()
+a.can_build()
+a.can_breathe()
+
+d = Doctor()
+d.can_cure()
+d.can_walk()
+d.can_breathe()
+
+
+
+
+
--- BeautifulSoup/Beautifulsoup.py
+++ BeautifulSoup/Beautifulsoup.py
@@ -1,26 +1,24 @@
 # Beautifulsoup
 from bs4 import BeautifulSoup
+import requests
+import random
 
-with open("files/1.html", encoding="utf8") as file:
-    src = file.read()
 
-# print(src)
+user_agent_list = [
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246',
+        'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
+        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36',
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:15.0) Gecko/20100101 Firefox/15.0.1',
+        'Mozilla/5.0 (CrKey armv7l 1.5.16041) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.0 Safari/537.36'
+]
 
-soup = BeautifulSoup(src, 'lxml')
-title = soup.title
-print(title.text)
-# print(title.string)
 
-# .find() find_all()
-# page_h1 = soup.findAll("meta")
-# for entry in page_h1:
-#    print(entry)
+#url = "https://automonth.ru/marki-avtomobilej/"
+url = "https://quotes.toscrape.com/"
+user_agent = random.choice(user_agent_list)
+headers = {'User agent': user_agent}
+response = requests.get(url=url, headers=headers)
+soup = BeautifulSoup(response.text, 'lxml')
 
-# news = soup.find_all("li", class_ = "even")
-# for entry in news:
-#     print(entry.text.strip())
-
-# news = soup.find_all("div", {"itemList", "itemListSecondary","catItemTitle", "catItemIntroText" })
-news = soup.find_all("div", class_="catItemIntroText")
-for entry in news:
-    print(entry.text.strip())
+print(soup)
