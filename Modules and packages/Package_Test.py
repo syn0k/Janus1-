892d5a8c92e28d369e40c265071723c47294ffc8
syn0k <yan.bekker@list.ru>
2021-12-06 12:41:40 +0200

    add __init__ example

--- Modules and packages/Package_Test.py
+++ Modules and packages/Package_Test.py
@@ -1,5 +1,5 @@
-from MainPackage import main_script
-from MainPackage.SubPackage import sub_script
+from MainPackage import main_script # as, and * - all modules
+from MainPackage.SubPackage import sub_script # as, and * - all modules
 
 main_script.hello_main()
 sub_script.hello_subscript()
---
+++ __name__ and __main__/converter.py
@@ -0,0 +1,16 @@
+print("Converter call file")
+
+def to_inches(cm):
+    return cm*0.393701
+
+def to_miles(km):
+    return km*0.621371
+
+def to_fahrenheit(celsius):
+    return (celsius*9/5)+32
+
+
+if __name__ == "__name__":
+    print("Call a converting func that you want")
+else:
+    print("Was imported (not executed directly)")
