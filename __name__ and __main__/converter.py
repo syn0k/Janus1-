8d279635063d4479657845ccd83bbc9f9d206f07
syn0k <yan.bekker@list.ru>
2021-12-06 16:56:28 +0200

    add example

--- __name__ and __main__/converter.py
+++ __name__ and __main__/converter.py
@@ -10,7 +10,7 @@ def to_fahrenheit(celsius):
     return (celsius*9/5)+32
 
 
-if __name__ == "__name__":
+if __name__ == "__main__":
     print("Call a converting func that you want")
 else:
     print("Was imported (not executed directly)")
