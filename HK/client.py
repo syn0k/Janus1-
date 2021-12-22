42eca7b6108030cd0d4ee6608523bf7b1eea43f5
syn0k <yan.bekker@list.ru>
2021-12-22 23:09:51 +0300

    edit end add aes

--- HK/client.py
+++ HK/client.py
@@ -1,17 +1,20 @@
 import socket
-import aes_
-
-
 
 sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
     sc.connect(("127.0.0.1", 54321))
     print("Connection esteblished to server")
-    message = sc.recv(1024)
-    print(message)
-    answer = "Hello back"
-    bytes = bytes(answer, 'utf-8')
-    sc.send(bytes)
+    while True:
+        message = sc.recv(1024)
+        print(message.decode("utf-8"))
+        exit1 = message.decode("utf-8")
+        if exit1 == "q":
+            break
+        else:
+            message_back = input("Type message to send to server: ")
+            #answer = "Hello back"
+            bytes = bytes(message_back, 'utf-8')
+            sc.send(bytes)
     sc.close()
 except:
     print("Connection fail")
--- HK/server.py
+++ HK/server.py
@@ -1,5 +1,4 @@
 import socket
-import aes_
 
 sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -7,10 +6,17 @@ sc.bind(("127.0.0.1", 54321))
 sc.listen(5)
 print("Listening for Incoming connection")
 target, ip = sc.accept()
-print("Target connected", target)
-message = input("* Shell~%s:" % str(ip))
-bytes = bytes(message, 'utf-8')
-target.send(bytes)
-answer = target.recv(1024)
-print(answer)
+print("Target connected")
+while True:
+    message = input("* Shell ~%s:" % str(ip))
+    bytes1 = bytes(message, 'utf-8')
+    exit1 = bytes1.decode("utf-8")
+    target.send(bytes1)
+
+    if exit1 == "q":
+        break
+    else:
+        answer = target.recv(1024)
+        exit1 = answer.decode("utf-8")
+        print(exit1)
 sc.close()
